957061b4c7ef934180749cc90015a3a158b3d2e4
Yusuke Morishita <yeemyu@debian-g41.bit-life.net>
2015-05-09 14:33:35 +0900

    Committer: Yusuke Morishita <yeemyu@debian-g41.bit-life.net>
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    	new file:   gtptest.py

---
+++ gtptest.py
@@ -0,0 +1,64 @@
+import os, sys, time
+
+def run_gtp():
+    os.execlp('gnugo', 'gnugo', '--mode', 'gtp')
+
+def operate():
+    ope_fh = open('operate.log', 'w')
+    black_res = ''
+    white_res = ''
+
+    print 'boardsize 9'
+    sys.stdout.flush()
+    temp = raw_input()
+    temp = raw_input()
+    
+    while 1:
+        print 'genmove black'
+        sys.stdout.flush()
+        black_res = raw_input()
+        ope_fh.write('black_res '+ black_res + '\n')
+        if black_res == '= resign' \
+           or (black_res == '= PASS' and white_res == '= PASS'):
+            temp = raw_input()
+            break
+        temp = raw_input()
+
+        print 'genmove white'
+        sys.stdout.flush()
+        white_res = raw_input()
+        ope_fh.write('white_res ' + white_res + '\n')
+        if white_res == '= resign' \
+           or (black_res == '= PASS' and white_res == '= PASS'):
+            temp = raw_input()
+            break
+        temp = raw_input()
+
+    print 'final_score'
+    sys.stdout.flush()
+    temp = raw_input()
+    ope_fh.write('score' + temp + '\n')
+
+    print 'quit'
+    sys.stdout.flush()
+    ope_fh.close()
+
+stdin = sys.stdin.fileno()
+stdout = sys.stdout.fileno()
+p_stdin, c_stdout = os.pipe()
+c_stdin, p_stdout = os.pipe()
+pid = os.fork()
+if pid:
+    # parent
+    os.close(c_stdout)
+    os.close(c_stdin)
+    os.dup2(p_stdin, stdin)
+    os.dup2(p_stdout, stdout)
+    operate()
+else:
+    # child
+    os.close(p_stdin)
+    os.close(p_stdout)
+    os.dup2(c_stdin, stdin)
+    os.dup2(c_stdout, stdout)
+    run_gtp()
